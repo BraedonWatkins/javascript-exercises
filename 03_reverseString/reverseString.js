9788495d61fa581211c2b4ed955dc94944796380
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-15 15:12:38 -0400

    passed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(inStr) {
+    retStr = '';
 
+    for(i = inStr.length - 1; i >= 0; i--){ retStr += inStr[i]; }
+
+    return retStr;
 };
 
 // Do not edit below this line
