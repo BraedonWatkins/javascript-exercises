9b2fb3ba547dd7d9dca2fdb0b9eccb592f4da546
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-15 15:57:29 -0400

    passed removedFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
 const removeFromArray = function() {
+    inArr = arguments[0];
 
+    for(j = 1; j < arguments.length; j++)
+    {
+        for(i = 0; i < inArr.length; i++)
+        {
+            if(inArr[i] === arguments[j]) {inArr.splice(i,1);}
+        }        
+    }
+
+    return inArr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
