b2180412ecb66509040f62c0d8f41069817e44fb
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-15 16:32:59 -0400

    tempConversion passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
+// [°F] = [°C] × 9⁄5 + 32 	
+// [°C] = ([°F] − 32) × 5⁄9
 
+const ftoc = function(inTemp) {
+    retTemp = inTemp - 32;
+    retTemp = Math.round((retTemp * 5 / 9) * 10) / 10
+    return retTemp;
 };
 
-const ctof = function() {
+/* Math notes:
+   66.66666666667
+   666.6666666667
 
+   Math.round...
+   666.0000000000
+   66.60000000000
+*/
+
+const ctof = function(inTemp) {
+    retTemp = Math.round((inTemp * 9 / 5) * 10) / 10
+    retTemp += 32;
+    return retTemp;
 };
 
 // Do not edit below this line
