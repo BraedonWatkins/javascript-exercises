4ff65e8befafab3e0833fed130d6be357ff60f2f
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-15 14:43:28 -0400

    passed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inStr, n) {
+    
+    if(typeof(inStr) !== 'string'){return "ERROR";}
+    if(n < 0) {return "ERROR";}
+    
+    retStr = '';
 
+    for (i = 0; i < n; i++)
+    {
+        retStr += inStr;
+    }
+
+    return retStr;
 };
 
 // Do not edit below this line
